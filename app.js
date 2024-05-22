d1dc9d871aa9ca158d04dc35493f5d11ae3feec0
Yousri <yoel-bas@e2r6p14.1337.ma>
2024-05-22 19:49:58 +0100

    m1

--- app.js
+++ app.js
@@ -4,6 +4,36 @@ function build_lay(one, two, container){
     container.append(two)
 }
 
+
+
+async function set()
+{ 
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2FhMTgyNDIwMDQ2M2Q4MjU0YTc1YzEwODE2NWQyOSIsInN1YiI6IjY2NGRlYzQwNjE4NmRlZGQ3YzA2MTIyZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C4pkljTBym5mxAnOKvFF509JyMq0leCBwbNl68c7f_U'
+        }
+    };
+    try {
+        const body = document.body;
+        const homeResp = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options);
+        if (homeResp.ok) {
+            var data_home = await homeResp.json();
+            // console.log(data_home);
+            console.log(data_home.results[0].backdrop_path);
+            for (var i = 0; i < data_home.length; i++) {
+                console.log(data_home.results[i].title);
+            }
+            body.style.background = `url(https://image.tmdb.org/t/p/original${data_home.results[0].backdrop_path})`;
+            body.style.position = absolute;
+        }
+    } 
+        catch (error) {
+            console.log(error);
+        }
+}
+set()
 async function getM()
 {
     const options = {
@@ -33,10 +63,14 @@ async function getM()
                 const new_div = document.createElement('div')
                 new_div.id = "container";
                 var new_poster = document.createElement('img');
+                if(data.results[i].adult == true)
+{                    i++;
+                    continue;}
+
                 if (!data.results[i].poster_path)
                 {
-                        i++;
-                        continue;
+                    i++;
+                    continue;
                 }
                 new_poster.src = "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
                 new_poster.style.width = "100%"
@@ -53,8 +87,9 @@ async function getM()
                 new_div.style.alignItems = "center";
                 new_div.style.padding = "1%"
                 new_div.style.cursor = "pointer";
+                new_div.style.padding = "10px"
                 new_div.addEventListener("mouseover", function(){
-                    new_div.style.transform = "scale(1.09)";
+                    new_div.style.transform = "scale(1.1)";
                 });
                 new_div.addEventListener("mouseout", function(){
                     new_div.style.transform = "scale(1)";
